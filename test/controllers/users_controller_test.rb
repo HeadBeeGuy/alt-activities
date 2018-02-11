0362ab8fbfa5c761e0260a9c3c1963353f71f49a
Jake <jwhiton@gmail.com>
2018-02-11 07:42:07 +0000

    Add in a very basic (and broken) user test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # conflicting information on the internet about whether to use ControllerHelpers or IntegrationHelpers
+  # it's a bit hard to search for stuff since questions often apply to older versions of Devise and Rails
+  #include Devise::Test::IntegrationHelpers
+  
+  def setup
+    #@user = users(:regular_user)
+  end
+  
+  test "a user with valid credentials should be able to log in" do
+    #not sure if this is necessary or not, it keeps complaining about "env"
+    #@request.env['devise.mapping'] = Devise.mappings[:user]
+    #sign_in @user
+    #assert_response :success
+  end
 end
