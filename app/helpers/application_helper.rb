705e59b3a04b0fd87167d69ce024880778ef88f7
Jake <jwhiton@gmail.com>
2018-03-22 10:32:25 +0000

    Add Markdown support to a few more views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 require 'redcarpet'
 
+# this comes from https://github.com/vmg/redcarpet/issues/92#issuecomment-35462000
+# it works, but I can't figure out how to incorporate the options hash down below, which has useful functionality
+# at some point this needs to be integrated, because Redcarpet surrounds its output in <p> tags, which causes unnecessary line breaks
+#class RenderWithoutWrap < Redcarpet::Render::HTML
+#  def postprocess(full_document)
+#    Regexp.new(/\A<p>(.*)<\/p>\Z/m).match(full_document)[1] rescue full_document
+#  end
+#end
+
 module ApplicationHelper
   
   # specifying options for Markdown rendering with Redcarpet
@@ -11,6 +20,7 @@ module ApplicationHelper
       hard_wrap: true,
       fenced_code_blocks: true,
       no_links: true,
+      no_images: true,
       no_styles: true
     }
     
@@ -25,6 +35,9 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
     
+    #markdown = Redcarpet::Markdown.new(RenderWithoutWrap, extensions)
+    
     markdown.render(text).html_safe
   end
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,4 +6,10 @@ class Tag < ApplicationRecord
   validates :short_name, presence: true, length: { maximum: 40 }, uniqueness: true
   validates :long_name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :description, presence: true, length: { maximum: 100 }
+  
+  # Adding this because the activity creation form uses collection_check_boxes to display a checkbox for each tag in each category
+  # It needs a symbol to pass in to generate the "label"
+  def name_for_lists
+    "#{long_name} - #{description}"
+  end
 end
