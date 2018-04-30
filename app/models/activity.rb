a59933c3a7024d6730f814dd617663da1145335c
HeadBeeGuy <jake.alt.dev@gmail.com>
2018-05-01 06:36:18 +0900

    Increase activity character limit to 3000

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,7 +9,7 @@ class Activity < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 50 }
   validates :short_description, presence: true, length: { maximum: 200 }
-  validates :long_description, presence: true, length: { maximum: 2000 } # need to see how feasible this is in practice!
+  validates :long_description, presence: true, length: { maximum: 3000 } # need to see how feasible this is in practice!
   #validates :tags, presence: true # I want to validate them, but it makes application submission break
 	validates :upvote_count, numericality: { greater_than_or_equal_to: 0 }
   
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -36,8 +36,8 @@ class ActivityTest < ActiveSupport::TestCase
     assert_not @activity.valid?
   end
   
-  test "an activity's long description can't be over 2000 characters" do
-    @activity.long_description = "a" * 2001
+  test "an activity's long description can't be over 3000 characters" do
+    @activity.long_description = "a" * 3001
     assert_not @activity.valid?
   end
   
