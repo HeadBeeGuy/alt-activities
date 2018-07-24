c66829d70813e174ce2790ba9069892bbe9cc7a2
HeadBeeGuy <jake.alt.dev@gmail.com>
2018-07-24 15:03:59 +0900

    Remove some extraneous entries in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
 	devise_for :users
 	root 'site_pages#home'
 
