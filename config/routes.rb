125afb60cd4f7e72e326eb1a729b9b6624b49908
HeadBeeGuy <jake.alt.dev@gmail.com>
2018-07-25 12:34:11 +0900

    Add a thanks page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 	get '/altto', to: 'site_pages#altto'
   get '/contact', to: 'site_pages#contact'
   get '/resources', to: 'site_pages#resources'
+  get '/thanks', to: 'site_pages#thanks'
 	get '/modqueue', to: 'activities#modqueue'
 	get '/all_tags', to: 'tag_categories#index'
   get '/posts/:id', to: 'front_page_posts#show', as: :posts
