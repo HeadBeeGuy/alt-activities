f5053796f926674e2199dad2664331ebfff9e590
HeadBeeGuy <jake.alt.dev@gmail.com>
2018-09-20 14:30:44 +0900

    Add in the rudiments of a function that will enable a search for activities with multiple tags

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -15,4 +15,30 @@ class Activity < ApplicationRecord
 	validates :upvote_count, numericality: { greater_than_or_equal_to: 0 }
   
   enum status: [:unapproved, :approved, :edited]
+
+  # adapted from https://stackoverflow.com/a/33558154
+  # and https://stackoverflow.com/a/11299725
+  # search_tags must be an array of integers, specifically the IDs of the tags
+  # this is likely an inept way to do this, but upon much searching, it appears that
+  # SQL intersects aren't part of ActiveRecord. How I wish they was!
+  # this works in Postgres, but who knows if it will work with other DBs
+  def self.find_with_all_tags(search_tags)
+    intersect_query = ""
+    search_tags.each_with_index do |tag_id, i|
+      return nil unless tag_id.is_a? Integer # will this actually thwart any sort of injection attack?
+      intersect_query += "SELECT activity_id FROM taggings WHERE tag_id = ?"
+      intersect_query += " INTERSECT " if i != search_tags.length - 1
+    end
+
+    # Constructing an array of Activity objects - will this hog a lot of memory?
+    # extracting activities is clunky, but unless I queried Tagging, it wouldn't return results
+    # I doubt this query will scale well, but I'm not anticipating that the tags will get too numerous
+    activity_array = []
+    Tagging.find_by_sql([intersect_query, *search_tags]).each do |tagging|
+      activity_array << tagging.activity
+    end
+
+    activity_array
+  end
+
 end
