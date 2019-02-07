039ba61b956f862d7541f1d93e9be021e6da4c4b
HeadBeeGuy <jake.alt.dev@gmail.com>
2019-02-07 12:35:15 +0900

    Change activity search query string to ILIKE so searches are case-insensitive

--- app/controllers/activity_searches_controller.rb
+++ app/controllers/activity_searches_controller.rb
@@ -3,8 +3,8 @@ class ActivitySearchesController < ApplicationController
 
   def show
     if params[:search]
-      @activities = Activity.where('name LIKE ? OR short_description LIKE ? OR ' +
-                                   'long_description LIKE ?', 
+      @activities = Activity.where('name ILIKE ? OR short_description ILIKE ? OR ' +
+                                   'long_description ILIKE ?', 
                                    "%#{params[:search]}%",
                                    "%#{params[:search]}%",
                                    "%#{params[:search]}%")
