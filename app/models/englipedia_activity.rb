6ff101c5ccd2b38c79e3fcb2480d51f2d9f6762b
HeadBeeGuy <jake.alt.dev@gmail.com>
2019-02-22 16:34:12 +0900

    Add some basic validations to EnglipediaActivity model

--- app/models/englipedia_activity.rb
+++ app/models/englipedia_activity.rb
@@ -1,2 +1,10 @@
 class EnglipediaActivity < ApplicationRecord
+
+  # validations will be pretty loose since the seed data has lots of quirks and
+  # messy bits
+  validates :title, length: { maximum: 200 }
+  validates :author, length: { maximum: 200 }
+  validates :submission_date, length: { maximum: 200 }
+  validates :estimated_time, length: { maximum: 200 }
+  validates :description, length: { maximum: 10000 }
 end
