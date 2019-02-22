1b004938912ea2736675409aff76c43338fe016b
HeadBeeGuy <jake.alt.dev@gmail.com>
2019-02-22 16:28:09 +0900

    Improve views and controller for Englipedia Activities to edit and delete them

--- app/controllers/englipedia_activities_controller.rb
+++ app/controllers/englipedia_activities_controller.rb
@@ -1,3 +1,4 @@
+
 class EnglipediaActivitiesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
@@ -6,7 +7,14 @@ class EnglipediaActivitiesController < ApplicationController
   end
 
   def show
+    # the archive location may change in the future
+    englipedia_file_root = "http://englipedia.co/www.englipedia.net"
     @activity = EnglipediaActivity.find(params[:id])
+    # making it sub! modifies @activity's file list as well, but it doesn't
+    # seem to save it to the db. But if I take it off, it won't persist the
+    # substitution in @links. I think I'm missing an obvious Ruby pattern here,
+    # but I'll have to tinker with it later.
+    @links = @activity.attached_files.each { |link| link.sub!(/\.\./, englipedia_file_root) }
   end
 
   def edit
@@ -21,6 +29,13 @@ class EnglipediaActivitiesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @activity = EnglipediaActivity.find(params[:id])
+    @activity.destroy
+    flash[:success] = "Activity deleted."
+    redirect_to englipedia_activities_url
   end
 
   private
