54fa5493ef234707ce00c709c785fdba9397ba0d
HeadBeeGuy <jake.alt.dev@gmail.com>
2019-02-22 11:13:51 +0900

    Add edit/update functionality to Englipedia activities

--- app/controllers/englipedia_activities_controller.rb
+++ app/controllers/englipedia_activities_controller.rb
@@ -8,4 +8,28 @@ class EnglipediaActivitiesController < ApplicationController
   def show
     @activity = EnglipediaActivity.find(params[:id])
   end
+
+  def edit
+    @activity = EnglipediaActivity.find(params[:id])
+  end
+
+  def update
+    @activity = EnglipediaActivity.find(params[:id])
+    if @activity.update_attributes(englipedia_activity_params)
+      flash[:success] = "Updated activity info."
+      redirect_to @activity
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def englipedia_activity_params
+      params.require(:englipedia_activity).permit(:title, :author,
+                                                  :submission_date,
+                                                  :estimated_time, :speaking,
+                                                  :listening, :reading,
+                                                  :writing, :description)
+    end
+    
 end
