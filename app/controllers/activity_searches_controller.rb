8673ec266313f505ccc06d9343d1cecfeab6c4ee
HeadBeeGuy <jake.alt.dev@gmail.com>
2019-06-12 15:30:26 +0900

    Add tags into the site's text search

--- app/controllers/activity_searches_controller.rb
+++ app/controllers/activity_searches_controller.rb
@@ -10,6 +10,10 @@ class ActivitySearchesController < ApplicationController
                                    "%#{params[:search]}%")
                      .approved.select(:id, :name, :upvote_count, :short_description)
                      .limit(20).order(upvote_count: :desc)
+      @tags = Tag.where('short_name ILIKE ? OR long_name ILIKE ? OR ' +
+                        'description ILIKE ?', "%#{params[:search]}%",
+                        "%#{params[:search]}%", "%#{params[:search]}%")
+                          .limit(15)
     else
       @activities = nil
     end
