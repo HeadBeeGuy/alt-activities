d9a5cd5e48b9a6425cd49293c9fa1a9be6a3bcf0
tkopdx <63491466+tkopdx@users.noreply.github.com>
2021-02-09 16:08:50 +0900

    fixed duplicates on firefox, setCheckboxes fires twice on all page loads

--- app/assets/javascripts/tag_searches.js
+++ app/assets/javascripts/tag_searches.js
@@ -1,75 +1,94 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on("turbolinks:load", function() {
+function init() {
+  //tooltips
+  setTooltips();
+  //checkboxes
+  setCheckboxes();
+}
+
+function setTooltips() {
   //Initialize tooltips, taken from the vanilla Bootstrap docs
-  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
+  const tooltipTriggerList = [
+    ...document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+  ];
+  tooltipTriggerList.map(function (el) {
+    return new bootstrap.Tooltip(el);
   });
-})
+}
 
-$(window).on("pageshow", function () {
+function setCheckboxes() {
+  const checkboxes = [...document.querySelectorAll("input.form-check-input")];
 
-  const checkboxes = [].slice.call(document.querySelectorAll('input.form-check-input'));
+  console.log(checkboxes.length);
 
   if (checkboxes.length) {
-    checkboxes.map(checkbox => {
+    checkboxes.map((checkbox) => {
+      console.log(checkbox.checked);
+
       if (checkbox.checked) {
-        const num = checkbox.parentNode.parentNode.parentNode.parentNode.parentNode.id.split('-')[2];
+        const num = checkbox.dataset.id;
         const ref = document.getElementById(`selected_items-${num}`);
-        const text = checkbox.parentNode.innerText;
+        const text = checkbox.dataset.text;
+        const curItems =
+          [...ref.childNodes].map((child) => child.innerText) || [];
 
-        checkbox.parentNode.classList.contains('btn-active') ? null : toggleAlertBorder(checkbox);
+        //on page load, if the checkbox is checked from a previous page instance then toggle the active class
+        checkbox.parentNode.classList.contains("btn-active")
+          ? null
+          : toggleAlertBorder(checkbox);
 
-        append(ref, text);
+        //Check to see if the list includes the text node before adding it to solve issues of duplicates on Firefox
+        curItems.includes(text) ? null : append(ref, text);
       }
-    })
-  }
-})
 
-const append =  (ref, text) => {
+      checkbox.addEventListener("click", handleTagClick);
+    });
+  }
+}
+
+const append = (ref, text) => {
   //Add an li element to the list in the category header
 
   const node = document.createElement("LI");
   const textnode = document.createTextNode(text);
-      
+
   node.appendChild(textnode);
 
   ref.appendChild(node);
-}
+};
 
-const getChildNodeIndex = function(children, text) {
+const getChildNodeIndex = function (children, text) {
   const arr = Array.from(children);
 
-  const index = arr.findIndex(li => {
+  const index = arr.findIndex((li) => {
     return li.innerText === text;
-  })
+  });
 
   return index;
-}
+};
 
-const handleTagClick = function(e) {
+const handleTagClick = function () {
+  const checked = this.checked;
+  const num = this.dataset.id;
+  const ref = document.getElementById(`selected_items-${num}`);
+  const text = this.dataset.text;
 
-    const checked = e.checked;
-    const num = e.parentNode.parentNode.parentNode.parentNode.parentNode.id.split('-')[2];
-    const ref = document.getElementById(`selected_items-${num}`);
-    const text = e.parentNode.innerText;
-    
-    if (checked) {
-      append(ref, text);
-    } else {
+  if (checked) {
+    append(ref, text);
+  } else {
+    const ind = getChildNodeIndex(ref.children, text);
+    ref.removeChild(ref.childNodes[ind]);
+  }
+  //toggle the border
+  toggleAlertBorder(this);
+};
 
-      const ind = getChildNodeIndex(ref.children, text);
+const toggleAlertBorder = function (e) {
+  e.parentNode.classList.toggle("btn-active");
+  e.parentNode.classList.toggle("btn-success");
+};
 
-      ref.removeChild(ref.childNodes[ind]);
-    }
-
-    toggleAlertBorder(e)
-    
-}
-
-const toggleAlertBorder = function(e) {
-  e.parentNode.classList.toggle('btn-active');
-  e.parentNode.classList.toggle('btn-success');
-}
+$(document).on("turbolinks:load", init);
+window.addEventListener('pageshow', setCheckboxes);
